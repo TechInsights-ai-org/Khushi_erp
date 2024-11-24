f22a55712bfbc990e8acfcd0d8d7966435f2da06
kaviraj <prkaviraj25@gmail.com>
2024-11-24 09:12:08 +0000

    Updated Doc String

--- khushi_erpnext/planning/doctype/planning/planning.py
+++ khushi_erpnext/planning/doctype/planning/planning.py
@@ -4,10 +4,10 @@
 import frappe
 from frappe.model.document import Document
 
-
 class Planning(Document):
 
 	def update_total_output(self) -> None:
+		""" Calculates the total quantity (output) """
 		if float(self.packing) == 0:
 			frappe.msgprint("Packing value cannot be zero.")
 			return
@@ -15,11 +15,21 @@ class Planning(Document):
 
 
 	def get_costing_value(self,field: str, fc:bool = False , vc:bool = False) -> float | int:
+		"""
+		Retrieves the total cost value for a specified field in the costing child table.
+		It aggregates values based on the field name and cost type (Fixed or Variable cost).
+		Args:
+		    field (str): The field name for which the costing value is calculated.
+		    fc (bool): If True, Return only SUM OF Fixed Costs (FC) .
+		    vc (bool): If True,Return only SUM OF Variable Costs (VC).
+		Returns:
+		    float | int: The calculated cost value (sum of the relevant field values).
+		"""
 		fc_total, vc_total, total = 0, 0, 0
 		costing_details : list = self.costing_sub_detail
 		for costing_detail in costing_details:
 			if hasattr(costing_detail, field) and costing_detail.count == 1:
-				value = getattr(costing_detail, field) or 0
+				value: int = getattr(costing_detail, field) or 0
 				if costing_detail.cost_type == "FC":
 					fc_total += value
 				elif costing_detail.cost_type == "VC":
@@ -32,6 +42,7 @@ class Planning(Document):
 		return total
 
 	def update_investment(self):
+		""" Updates the value for the fields that are in Investment Tab """
 		self.total_investment_bef_gst: float  = self.get_costing_value("amt_bef_tax")
 		self.total_investment_with_gst: float  = self.get_costing_value("total_amt")
 		self.fix_cost_bef_tax: float  = self.total_investment_bef_gst - self.get_costing_value("amt_bef_tax",fc=True,vc = False)
@@ -41,6 +52,7 @@ class Planning(Document):
 		self.input_gst: float = self.get_costing_value("gst_amount")
 
 	def update_cost_details(self):
+		""" Updates the value for the fields that are in cost details Tab """
 		if self.total_qnty == 0:
 			return
 		self.cost_with_gst: float = self.total_investment_with_gst / self.total_qnty
@@ -49,28 +61,27 @@ class Planning(Document):
 		self.fix_cost_without_gst:  float = self.fix_cost_bef_tax / self.total_qnty
 
 	def update_cost_table(self):
+		"""Updates the costing sub-details child table"""
 		cost_table : list = self.costing_sub_detail
 		if cost_table:
 			for cost_data in cost_table:
-				qnty = cost_data.qnty or 0
-				s1 = cost_data.s1 or 0
-				s2 = cost_data.s2 or 0
-				weight = cost_data.weight or 0
-				rate = cost_data.rate or 0
-				rate_unit = cost_data.rate_unit or 0
+				qnty: int | float = cost_data.qnty or 0
+				s1: int | float = cost_data.s1 or 0
+				s2: int | float = cost_data.s2 or 0
+				weight: int | float = cost_data.weight or 0
+				rate: int | float = cost_data.rate or 0
+				rate_unit: int | float = cost_data.rate_unit or 0
 				cost_data.amt_bef_tax: float = 0.00
 				if cost_data.rate_unit and cost_data.rate_unit !=0 :
-					cost_data.amt_bef_tax = (qnty * s1 * s2 * weight * rate) / rate_unit
+					cost_data.amt_bef_tax: float = (qnty * s1 * s2 * weight * rate) / rate_unit
 				if cost_data.gst:
-					gst_percentage = eval(cost_data.gst.replace("%",""))
+					gst_percentage: int | float = eval(cost_data.gst.replace("%",""))
 					if gst_percentage <= 9:
 						gst_percentage = gst_percentage/10
 					else:
 						gst_percentage = gst_percentage /100
-
 					cost_data.gst_amount: float = cost_data.amt_bef_tax * gst_percentage
 				cost_data.total_amt: float = cost_data.amt_bef_tax + cost_data.gst_amount
-
 
 	def before_save(self):
 		self.update_cost_table()
