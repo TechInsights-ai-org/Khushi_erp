d10f21505d5ad4ad78cb2852d20c447f786e8ac6
SAnnmalai-techfinite <sannmalai@techfinite.com>
2024-11-28 12:49:35 +0530

    Included the filter and added the colour formatting

--- khushi_erpnext/stock_customization/report/sales_order_vs_stock_balance_report/sales_order_vs_stock_balance_report.js
+++ khushi_erpnext/stock_customization/report/sales_order_vs_stock_balance_report/sales_order_vs_stock_balance_report.js
@@ -8,10 +8,25 @@ frappe.query_reports["Sales Order VS Stock Balance Report"] = {
 		{label: "To Date", fieldname: "t_date", fieldtype: "Date"},
 		{label: "Sales Order", fieldname: "sales_order", fieldtype: "Link", options: "Sales Order"},
 		{label: "Warehouse", fieldname: "warehouse", fieldtype: "Link", options: 'Warehouse'},
-		{label: "Status", fieldname: "status", fieldtype: "MultiSelectList","options": "Entity",
-			"get_data": function(txt) {
-				return ["Draft", "On Hold", "To Deliver and Bill", "To Bill", "To Deliver", "Completed", "Cancelled", "Closed"];
-			}
+		{label: "Status", fieldname: "status", fieldtype: "MultiSelectList", options: [{value: "Draft", description:""},
+				{value: "On Hold", description:""}, {value: "To Deliver and Bill", description:""},
+				{value: "To Bill", description:""}, {value: "To Deliver", description:""},
+				{value: "Completed", description:""}, {value: "Cancelled", description:""},
+				{value: "Closed", description:""}]
+		},
+		{label: "Group By Item", fieldname: "group_by_item", fieldtype: "Check"},
+	],
+	 "formatter":function (value, row, column, data, default_formatter) {
+		value = default_formatter(value, row, column, data);
+		if (column.id == "qty_to_deliver" && data["qty_to_deliver"] > data["stock_balance"] + data["purchase_qty"]) {
+			value = "<span style='color:red!important;font-weight:bold'>" + value + "</span>";
 		}
-	]
+		if (column.id == "qty_to_deliver" && data["qty_to_deliver"] > data["stock_balance"] && data["qty_to_deliver"] < data["stock_balance"] + data["purchase_qty"]) {
+			value = "<span style='color:yellow!important;font-weight:bold'>" + value + "</span>";
+		}
+		if (column.id == "purchase_qty" && data["qty_to_deliver"] > data["stock_balance"] && data["qty_to_deliver"] < data["stock_balance"] + data["purchase_qty"]) {
+			value = "<span style='color:red!important;font-weight:bold'>" + value + "</span>";
+		}
+		return value;
+    }
 };
--- khushi_erpnext/stock_customization/report/sales_order_vs_stock_balance_report/sales_order_vs_stock_balance_report.py
+++ khushi_erpnext/stock_customization/report/sales_order_vs_stock_balance_report/sales_order_vs_stock_balance_report.py
@@ -25,14 +25,67 @@ def get_columns() -> list[dict]:
 
 def get_condition(filters: dict) -> str:
     "Need to return condition based on the filter the give"
-    pass
+    cond: str = "WHERE so.status <> 'Cancelled'"
+    company: str = filters.get("company", "")
+    f_date: "str" = filters.get("f_date", "")
+    t_date: "str" = filters.get("t_date", "")
+    sales_order: str = filters.get("sales_order", "")
+    status: tuple = tuple(filters.get("status", []))
+    if company:
+        cond += f" AND so.company = '{company}'"
+    if f_date:
+        cond += f" AND so.transaction_date >= '{f_date}'"
+    if t_date:
+        cond += f" AND so.transaction_date <= '{t_date}'"
+    if sales_order:
+        cond += f" AND so.name = '{sales_order}'"
+    if status:
+        if len(status) == 1:
+            cond += f" AND so.status = '{status[0]}'"
+        else:
+            cond += f" AND so.status IN {status}"
+    return cond
+
+
+def get_group_by(filters: dict) -> str:
+    group_by: str = ""
+    group_by_item: int = filters.get("group_by_item", 0)
+    if group_by_item:
+        group_by += "GROUP BY soi.item_code"
+    return group_by
+
+
+def get_select_field(filters: dict) -> str:
+    select_fields = """so.transaction_date as "Date",so.name as "Sales Order",so.status as "Status", 
+                                so.customer as "Customer",
+                                soi.item_code as "Item Code",
+                                soi.qty as "Qty to Deliver" """
+    group_by_item: int = filters.get("group_by_item", 0)
+    if group_by_item:
+        select_fields = """GROUP_CONCAT(DISTINCT so.transaction_date) as "Date",
+                                        GROUP_CONCAT(DISTINCT so.name) as "Sales Order",
+                                        GROUP_CONCAT(DISTINCT so.status) as "Status", 
+                                        GROUP_CONCAT(DISTINCT so.customer) as "Customer",
+                                        soi.item_code as "Item Code",
+                                        SUM(soi.qty) as "Qty to Deliver" """
+    return select_fields
+
+
+def form_sql_query(filters: dict) -> str:
+    select_fields: str = get_select_field(filters)
+    cond: str = get_condition(filters)
+    group_by: str = get_group_by(filters)
+    query =f"""SELECT {select_fields} FROM `tabSales Order` AS so
+                                 JOIN `tabSales Order Item` AS soi ON so.name = soi.parent
+                                 {cond} {group_by}"""
+    return query
 
 
 @redis_cache
 def get_stock_balance(item_code: str, warehouse: str = None) -> float:
     cond: str = f"WHERE item_code = '{item_code}'"
     if warehouse:
-        cond += f" warehouse = {warehouse}"
+        cond += f" AND warehouse = '{warehouse}'"
     stock_qty: float = frappe.db.sql(f"""SELECT SUM(actual_qty) AS "stock_qty" FROM `tabBin` {cond};""",
                                    pluck="stock_qty")[0]
     return stock_qty if stock_qty else 0
@@ -55,16 +108,8 @@ def append_balance(func, data: list[list], *args):
 
 
 def get_data(filters: dict) -> list[list]:
-    cond: str = get_condition(filters)
-    data = frappe.db.sql("""SELECT so.transaction_date as "Date",
-                               so.name             as "Sales Order",
-                               so.status           as "Status",
-                               so.customer         as "Customer",
-                               soi.item_code       as "Item Code",
-                               soi.qty             as "Qty to Deliver"
-                        FROM `tabSales Order` AS so
-                                 JOIN `tabSales Order Item` AS soi ON so.name = soi.parent
-                        WHERE so.status <> "Cancelled";""", as_list=True)
+    query: str = form_sql_query(filters)
+    data = frappe.db.sql(query, as_list=True)
     data = append_balance(get_stock_balance, data, filters.get("warehouse", None))
     data = append_balance(get_po_balance, data)
     return data
